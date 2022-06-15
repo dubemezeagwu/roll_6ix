b4c938ed6a18536fc2e6309ff939308af38dd910
Yczar <babalolagbogo@gmail.com>
2022-06-15 09:29:08 +0100

    refactored the dice cubit

--- lib/bloc/dice_cubit.dart
+++ lib/bloc/dice_cubit.dart
@@ -10,29 +10,34 @@ part 'dice_state.dart';
 class DiceCubit extends Cubit<DiceState> {
   DiceCubit() : super(DiceInitialState());
 
-  final List<Player> listOfPlayers= [ playerOne,playerTwo];
-  late var winningStatus;
-  final gameResults = [];
+  Player? _player1;
+  Player? get player1 => _player1;
 
-  void startGameRound () {
-    gameResults.clear();
-    listOfPlayers.forEach((player) {
-      final playerNumber = Random().nextInt(6);
-      gameResults.add(playerNumber);
-      player.dice = AppAssets.diceArray[playerNumber];
-    });
-    print(gameResults);
-    displayWinner(gameResults);
+  ///
+  Player? _player2;
+  Player? get player2 => _player2;
+
+  ///
+  void startGameRound() {
+    final player1Number = Random().nextInt(6);
+    final player2Number = Random().nextInt(6);
+
+    _player1 = Player(
+      dice: AppAssets.diceArray[player1Number],
+      numberPlayed: player1Number,
+    );
+    _player2 = Player(
+      dice: AppAssets.diceArray[player2Number],
+      numberPlayed: player2Number,
+    );
+    if (_player1 == null || _player2 == null) {
+      // emit error state
+    }
+    emit(
+      DiceLoadedState(
+        player1: _player1!,
+        player2: player2!,
+      ),
+    );
   }
-
-  void displayWinner (List gameResults){
-    var winner = gameResults[0] != gameResults[1]
-        ? "Player ${gameResults.indexOf(gameResults.reduce((value, element) => max)) + 1 }"
-        : "It is a Tie";
-
-    winningStatus = winner;
-    print(winningStatus);
-  }
-
-  void emitDiceLoaded () => emit(DiceLoadedState(listOfPlayers));
 }
