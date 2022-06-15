a9c3ab4dccb6e044da850dfb18559d2f2dbb2517
Yczar <babalolagbogo@gmail.com>
2022-06-15 09:29:17 +0100

    refactore dice state

--- lib/bloc/dice_state.dart
+++ lib/bloc/dice_state.dart
@@ -1,17 +1,30 @@
 part of 'dice_cubit.dart';
 
-abstract class DiceState extends Equatable{}
+abstract class DiceState extends Equatable {}
 
-class DiceInitialState extends DiceState{
+class DiceInitialState extends DiceState {
   @override
   List<Object?> get props => [];
 }
 
 class DiceLoadedState extends DiceState {
+  final Player player1;
+  final Player player2;
+  DiceLoadedState({
+    required this.player1,
+    required this.player2,
+  });
 
-  final List<Player> players;
-  DiceLoadedState(this.players);
+  String get winningStatus {
+    return identical(player1, player2)
+        ? 'It is a Tie'
+        : player1.numberPlayed > player2.numberPlayed
+            ? 'Player one'
+            : 'Player two';
+    //  player1.  ? 'It is a Tie' :
+  }
 
+  // Player
   @override
-  List<Object?> get props => [players];
+  List<Object?> get props => [player1, player2];
 }
